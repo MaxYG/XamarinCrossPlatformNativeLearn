9c0ce986fdc99455e1bc062cc812314014825062
TomYangGang <yangg@shinetechchina.com>
2017-12-29 11:00:47 +0800

    finished splash  screen and change project name

---
+++ XamarinCrossPlatformNative/XamarinCrossPlatformNative.Android/SplashActivity.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+
+namespace XamarinCrossPlatformNative.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash",MainLauncher = true,NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        private static readonly string Tag = "X:" + typeof(SplashActivity).Name;
+        public  override void OnCreate(Bundle savedInstanceState,PersistableBundle persistableBundle)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Log.Debug(Tag, "SplashActivity.OnCreate");
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            var startupWork=new Task(SimulateStartup);
+            startupWork.Start();
+        }
+
+        public async void SimulateStartup()
+        {
+            Log.Debug(Tag, "Performing some startup work that takes a bit of time.");
+            await Task.Delay(3000);
+            Log.Debug(Tag, "Startup work is finished - starting MainActivity.");
+            StartActivity(new Intent(Application.Context,typeof(MainActivity)));
+        }
+
+        public override void OnBackPressed() { }
+    }
+}
