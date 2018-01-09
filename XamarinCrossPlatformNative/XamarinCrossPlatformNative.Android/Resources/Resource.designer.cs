86db987b326ca2071306aa7b4a3fe197f1a88d10
Tom <yangg@shinetechchina.com>
2018-01-09 18:27:01 +0800

    show list

--- XamarinCrossPlatformNative/XamarinCrossPlatformNative.Android/Resources/Resource.designer.cs
+++ XamarinCrossPlatformNative/XamarinCrossPlatformNative.Android/Resources/Resource.designer.cs
@@ -1723,8 +1723,8 @@ namespace XamarinCrossPlatformNative.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0a005d
-			public const int action0 = 2131361885;
+			// aapt resource value: 0x7f0a005e
+			public const int action0 = 2131361886;
 			
 			// aapt resource value: 0x7f0a0047
 			public const int action_bar = 2131361863;
@@ -1747,17 +1747,17 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a0024
 			public const int action_bar_title = 2131361828;
 			
-			// aapt resource value: 0x7f0a005a
-			public const int action_container = 2131361882;
+			// aapt resource value: 0x7f0a005b
+			public const int action_container = 2131361883;
 			
 			// aapt resource value: 0x7f0a0048
 			public const int action_context_bar = 2131361864;
 			
-			// aapt resource value: 0x7f0a0061
-			public const int action_divider = 2131361889;
+			// aapt resource value: 0x7f0a0062
+			public const int action_divider = 2131361890;
 			
-			// aapt resource value: 0x7f0a005b
-			public const int action_image = 2131361883;
+			// aapt resource value: 0x7f0a005c
+			public const int action_image = 2131361884;
 			
 			// aapt resource value: 0x7f0a0002
 			public const int action_menu_divider = 2131361794;
@@ -1774,11 +1774,11 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a0026
 			public const int action_mode_close_button = 2131361830;
 			
-			// aapt resource value: 0x7f0a005c
-			public const int action_text = 2131361884;
+			// aapt resource value: 0x7f0a005d
+			public const int action_text = 2131361885;
 			
-			// aapt resource value: 0x7f0a006a
-			public const int actions = 2131361898;
+			// aapt resource value: 0x7f0a006b
+			public const int actions = 2131361899;
 			
 			// aapt resource value: 0x7f0a0027
 			public const int activity_chooser_view_content = 2131361831;
@@ -1801,14 +1801,14 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a002e
 			public const int buttonPanel = 2131361838;
 			
-			// aapt resource value: 0x7f0a005e
-			public const int cancel_action = 2131361886;
+			// aapt resource value: 0x7f0a005f
+			public const int cancel_action = 2131361887;
 			
 			// aapt resource value: 0x7f0a003e
 			public const int checkbox = 2131361854;
 			
-			// aapt resource value: 0x7f0a0066
-			public const int chronometer = 2131361894;
+			// aapt resource value: 0x7f0a0067
+			public const int chronometer = 2131361895;
 			
 			// aapt resource value: 0x7f0a001e
 			public const int collapseActionView = 2131361822;
@@ -1837,8 +1837,8 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a001b
 			public const int end = 2131361819;
 			
-			// aapt resource value: 0x7f0a0070
-			public const int end_padder = 2131361904;
+			// aapt resource value: 0x7f0a0071
+			public const int end_padder = 2131361905;
 			
 			// aapt resource value: 0x7f0a0028
 			public const int expand_activities_button = 2131361832;
@@ -1855,8 +1855,8 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a002c
 			public const int icon = 2131361836;
 			
-			// aapt resource value: 0x7f0a006b
-			public const int icon_group = 2131361899;
+			// aapt resource value: 0x7f0a006c
+			public const int icon_group = 2131361900;
 			
 			// aapt resource value: 0x7f0a001f
 			public const int ifRoom = 2131361823;
@@ -1867,14 +1867,14 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a0056
 			public const int imageView1 = 2131361878;
 			
-			// aapt resource value: 0x7f0a0067
-			public const int info = 2131361895;
+			// aapt resource value: 0x7f0a0068
+			public const int info = 2131361896;
 			
-			// aapt resource value: 0x7f0a006c
-			public const int line1 = 2131361900;
+			// aapt resource value: 0x7f0a006d
+			public const int line1 = 2131361901;
 			
-			// aapt resource value: 0x7f0a006e
-			public const int line3 = 2131361902;
+			// aapt resource value: 0x7f0a006f
+			public const int line3 = 2131361903;
 			
 			// aapt resource value: 0x7f0a0057
 			public const int linearLayout1 = 2131361879;
@@ -1885,32 +1885,35 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a002b
 			public const int list_item = 2131361835;
 			
-			// aapt resource value: 0x7f0a0060
-			public const int media_actions = 2131361888;
-			
-			// aapt resource value: 0x7f0a0074
-			public const int menu_copy = 2131361908;
-			
-			// aapt resource value: 0x7f0a0073
-			public const int menu_cut = 2131361907;
-			
-			// aapt resource value: 0x7f0a0077
-			public const int menu_edit = 2131361911;
+			// aapt resource value: 0x7f0a0061
+			public const int media_actions = 2131361889;
 			
 			// aapt resource value: 0x7f0a0075
-			public const int menu_paste = 2131361909;
+			public const int menu_copy = 2131361909;
 			
-			// aapt resource value: 0x7f0a0079
-			public const int menu_preferences = 2131361913;
+			// aapt resource value: 0x7f0a0074
+			public const int menu_cut = 2131361908;
 			
 			// aapt resource value: 0x7f0a0078
-			public const int menu_save = 2131361912;
+			public const int menu_edit = 2131361912;
+			
+			// aapt resource value: 0x7f0a0076
+			public const int menu_paste = 2131361910;
+			
+			// aapt resource value: 0x7f0a007a
+			public const int menu_preferences = 2131361914;
+			
+			// aapt resource value: 0x7f0a0079
+			public const int menu_save = 2131361913;
 			
 			// aapt resource value: 0x7f0a001c
 			public const int middle = 2131361820;
 			
 			// aapt resource value: 0x7f0a0013
 			public const int multiply = 2131361811;
+			
+			// aapt resource value: 0x7f0a005a
+			public const int myListView = 2131361882;
 			
 			// aapt resource value: 0x7f0a0020
 			public const int never = 2131361824;
@@ -1921,14 +1924,14 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a000a
 			public const int normal = 2131361802;
 			
-			// aapt resource value: 0x7f0a0069
-			public const int notification_background = 2131361897;
+			// aapt resource value: 0x7f0a006a
+			public const int notification_background = 2131361898;
+			
+			// aapt resource value: 0x7f0a0064
+			public const int notification_main_column = 2131361892;
 			
 			// aapt resource value: 0x7f0a0063
-			public const int notification_main_column = 2131361891;
-			
-			// aapt resource value: 0x7f0a0062
-			public const int notification_main_column_container = 2131361890;
+			public const int notification_main_column_container = 2131361891;
 			
 			// aapt resource value: 0x7f0a0030
 			public const int parentPanel = 2131361840;
@@ -1942,14 +1945,14 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a0040
 			public const int radio = 2131361856;
 			
-			// aapt resource value: 0x7f0a0068
-			public const int right_icon = 2131361896;
+			// aapt resource value: 0x7f0a0069
+			public const int right_icon = 2131361897;
 			
-			// aapt resource value: 0x7f0a0064
-			public const int right_side = 2131361892;
+			// aapt resource value: 0x7f0a0065
+			public const int right_side = 2131361893;
 			
-			// aapt resource value: 0x7f0a0071
-			public const int sampleTextView = 2131361905;
+			// aapt resource value: 0x7f0a0072
+			public const int sampleTextView = 2131361906;
 			
 			// aapt resource value: 0x7f0a0014
 			public const int screen = 2131361812;
@@ -1996,8 +1999,8 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a0055
 			public const int select_dialog_listview = 2131361877;
 			
-			// aapt resource value: 0x7f0a0076
-			public const int shareMenuItem = 2131361910;
+			// aapt resource value: 0x7f0a0077
+			public const int shareMenuItem = 2131361911;
 			
 			// aapt resource value: 0x7f0a003f
 			public const int shortcut = 2131361855;
@@ -2026,8 +2029,8 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a0017
 			public const int src_over = 2131361815;
 			
-			// aapt resource value: 0x7f0a005f
-			public const int status_bar_latest_event_content = 2131361887;
+			// aapt resource value: 0x7f0a0060
+			public const int status_bar_latest_event_content = 2131361888;
 			
 			// aapt resource value: 0x7f0a0041
 			public const int submenuarrow = 2131361857;
@@ -2038,11 +2041,11 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a000b
 			public const int tabMode = 2131361803;
 			
-			// aapt resource value: 0x7f0a006f
-			public const int text = 2131361903;
+			// aapt resource value: 0x7f0a0070
+			public const int text = 2131361904;
 			
-			// aapt resource value: 0x7f0a006d
-			public const int text2 = 2131361901;
+			// aapt resource value: 0x7f0a006e
+			public const int text2 = 2131361902;
 			
 			// aapt resource value: 0x7f0a0035
 			public const int textSpacerNoButtons = 2131361845;
@@ -2056,8 +2059,8 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a0059
 			public const int textView2 = 2131361881;
 			
-			// aapt resource value: 0x7f0a0065
-			public const int time = 2131361893;
+			// aapt resource value: 0x7f0a0066
+			public const int time = 2131361894;
 			
 			// aapt resource value: 0x7f0a002d
 			public const int title = 2131361837;
@@ -2068,8 +2071,8 @@ namespace XamarinCrossPlatformNative.Droid
 			// aapt resource value: 0x7f0a003a
 			public const int title_template = 2131361850;
 			
-			// aapt resource value: 0x7f0a0072
-			public const int toolbar = 2131361906;
+			// aapt resource value: 0x7f0a0073
+			public const int toolbar = 2131361907;
 			
 			// aapt resource value: 0x7f0a0023
 			public const int top = 2131361827;
